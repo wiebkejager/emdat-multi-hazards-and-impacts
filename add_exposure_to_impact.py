93a6614ef6225d9929b7853d5661de7e4a199c92
Jäger <w.s.jaeger@vu.nl>
2023-06-16 17:00:13 +0200

    add combined impact and vulnerability

---
+++ add_exposure_to_impact.py
@@ -0,0 +1,42 @@
+# %% Imports
+import geopandas as gpd
+import rasterstats
+import rioxarray
+import rasterio as rio
+
+# %%
+START_YEAR = 2000
+END_YEAR = 2015
+EXPOSURE_PATH_1 = "C:/Users/wja209/DATA/RAW/WorldPop/"
+PROCESSED_IMPACT_AND_VUL_PATH = (
+    "C:/Users/wja209/DATA/PROCESSED/impact_vulnerability_1990_2015.gpkg"
+)
+PROCESSED_IMPACT_EXP_AND_VUL_PATH = (
+    "C:/Users/wja209/DATA/PROCESSED/impact_exposure_vulnerability_1990_2015.gpkg"
+)
+
+# %%
+gdf_impact = gpd.read_file(PROCESSED_IMPACT_AND_VUL_PATH)
+
+# %%
+impact_years = gdf_impact["Start Date"].dt.year.dropna().unique()
+
+# %% Add population count
+for year in impact_years:
+    print(year)
+    EXPOSURE_PATH = EXPOSURE_PATH_1 + "ppp_" + str(int(year)) + "_1km_Aggregated.tif"
+    ds_exp = rio.open(EXPOSURE_PATH)
+    year_filter = gdf_impact["Start Date"].dt.year == year
+    gdf_impact.loc[year_filter, "Population Count"] = [
+        x["sum"]
+        for x in rasterstats.zonal_stats(
+            gdf_impact.loc[year_filter, "geometry"],
+            ds_exp.read(1),
+            nodata=ds_exp.nodata,
+            affine=ds_exp.transform,
+            stats="sum",
+        )
+    ]
+
+# %% Save merged file
+gdf_impact.to_file(PROCESSED_IMPACT_EXP_AND_VUL_PATH)
