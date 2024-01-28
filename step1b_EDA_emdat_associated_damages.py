2249c04b25cbcdbbd70368f68d8644c4b540f17f
Jäger <w.s.jaeger@vu.nl>
2024-01-28 08:34:10 +0100

    fix step1b

--- step1b_EDA_emdat_associated_damages.py
+++ step1b_EDA_emdat_associated_damages.py
@@ -4,10 +4,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import missingno as msno
+
 from my_functions import get_bs_sample_df, get_impact_mean
 
 # %% Constants
-PROCESSED_EMDAT_PATH = "C:/Users/wja209/DATA/PROCESSED/emdat_2000_2015.csv"
+PROCESSED_EMDAT_PATH = "data/emdat_2000_2018.csv"
 
 # %%
 df = pd.read_csv(PROCESSED_EMDAT_PATH).set_index("Dis No")
