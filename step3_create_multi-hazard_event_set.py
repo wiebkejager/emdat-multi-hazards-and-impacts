c86e2f33cd8b1f86bab77629a68d46d0e7cf30c2
Jäger <w.s.jaeger@vu.nl>
2024-01-28 10:04:03 +0100

    step3

--- step3_create_multi-hazard_event_set.py
+++
@@ -1,9 +0,0 @@
-# %% Imports
-import pandas as pd
-import datetime
-import json
-
-
-df_s_t_overlapping_events = pd.read_csv(
-    "data/df_s_t_overlapping_events.csv", sep=";", index_col=0
-)
--- step3_create_single_and_double_hazard_ events_set.py
+++ step3_create_single_and_double_hazard_ events_set.py
@@ -3,18 +3,23 @@ import pandas as pd
 import numpy as np
 import json
 
-# %%
-PROCESSED_IMPACT_PATH_CSV = "data/impact_2000_2018.csv"
 
-# %%
-df_emdat = pd.read_csv(PROCESSED_IMPACT_PATH_CSV, sep=";").set_index("Dis No")
-df_emdat[["Hazard1", "Hazard2", "Hazard3"]] = df_emdat[
+# %% Constants
+PROCESSED_UNIQUE_IMPACT_PATH_CSV = "data/unique_events_impact_2000_2018.csv"
+
+
+# %% Load impact data
+df_impact = pd.read_csv(PROCESSED_UNIQUE_IMPACT_PATH_CSV, sep=";", index_col=0)
+df_impact[["Hazard1", "Hazard2", "Hazard3"]] = df_impact[
     ["Hazard1", "Hazard2", "Hazard3"]
 ].fillna("")
-# %%
-df_overlaps = pd.read_csv("df_res.csv", sep=";", index_col=0)
-df_overlaps.replace("[]", np.nan, inplace=True)
-df_overlaps.dropna(inplace=True)
+
+# %% Load overlapping event list and remove empty entries
+df_overlapping_events = pd.read_csv(
+    "data/df_s_t_overlapping_events.csv", sep=";", index_col=0
+)
+df_overlapping_events.replace("[]", np.nan, inplace=True)
+df_overlapping_events.dropna(inplace=True)
 
 
 # %%
@@ -47,9 +52,9 @@ df = pd.DataFrame(
 )
 
 # %%
-for ix, row in df_emdat.iterrows():
+for ix, row in df_impact.iterrows():
     # disaster does not have spatial overlap
-    if ix not in df_overlaps.index:
+    if ix not in df_overlapping_events.index:
         if row["Hazard3"] == "":
             new_row = pd.DataFrame(
                 [
@@ -93,8 +98,10 @@ for ix, row in df_emdat.iterrows():
             df = pd.concat([df, new_row], ignore_index=True)
     # disaster has spatial overlap and is single hazard:
     elif (row["Hazard2"] == "") & (row["Hazard3"] == ""):
-        disastersi = [ix] + json.loads(df_overlaps.loc[ix, "Overlapping events"])
-        df_tempi = df_emdat.loc[disastersi][["Start Date", "End Date"]].sort_values(
+        disastersi = [ix] + json.loads(
+            df_overlapping_events.loc[ix, "Overlapping events"]
+        )
+        df_tempi = df_impact.loc[disastersi][["Start Date", "End Date"]].sort_values(
             by="Start Date"
         )
         jx = df_tempi.index[0]
@@ -131,8 +138,8 @@ for ix, row in df_emdat.iterrows():
         #     df = pd.concat([df, new_row], ignore_index=True)
         if (
             (ix == df_tempi.index[1])
-            & (df_emdat.loc[jx, "Hazard2"] == "")
-            & (df_emdat.loc[jx, "Hazard3"] == "")
+            & (df_impact.loc[jx, "Hazard2"] == "")
+            & (df_impact.loc[jx, "Hazard3"] == "")
         ):  # if 2nd in overlapping hazards and 1st hazard is also single hazard-> add to dataframe as H2
             new_row = pd.DataFrame(
                 [
@@ -162,23 +169,25 @@ for ix, row in df_emdat.iterrows():
                 ],
                 columns=cols,
             )
-            if jx not in df_overlaps.index:
+            if jx not in df_overlapping_events.index:
                 continue
             # if 1st hazard has no other overlapping hazard before it add as H1 and add H1+H2
-            disastersj = [jx] + json.loads(df_overlaps.loc[jx, "Overlapping events"])
-            df_tempj = df_emdat.loc[disastersj][["Start Date", "End Date"]].sort_values(
-                by="Start Date"
+            disastersj = [jx] + json.loads(
+                df_overlapping_events.loc[jx, "Overlapping events"]
             )
+            df_tempj = df_impact.loc[disastersj][
+                ["Start Date", "End Date"]
+            ].sort_values(by="Start Date")
             if jx == df_tempj.index[0]:
                 new_row["Dis No 1"] = jx
-                new_row["Hazard 1"] = df_emdat.loc[jx, "Hazard1"]
-                new_row["Magnitude 1"] = df_emdat.loc[jx, "Dis Mag Value"]
-                new_row["Start Date 1"] = df_emdat.loc[jx, "Start Date"]
-                new_row["End Date 1"] = df_emdat.loc[jx, "End Date"]
-                new_row["Country 1"] = df_emdat.loc[jx, "Country"]
-                new_row["Total Deaths 1"] = df_emdat.loc[jx, "Total Deaths"]
-                new_row["Total Affected 1"] = df_emdat.loc[jx, "Total Affected"]
-                new_row["Total Damages 1"] = df_emdat.loc[
+                new_row["Hazard 1"] = df_impact.loc[jx, "Hazard1"]
+                new_row["Magnitude 1"] = df_impact.loc[jx, "Dis Mag Value"]
+                new_row["Start Date 1"] = df_impact.loc[jx, "Start Date"]
+                new_row["End Date 1"] = df_impact.loc[jx, "End Date"]
+                new_row["Country 1"] = df_impact.loc[jx, "Country"]
+                new_row["Total Deaths 1"] = df_impact.loc[jx, "Total Deaths"]
+                new_row["Total Affected 1"] = df_impact.loc[jx, "Total Affected"]
+                new_row["Total Damages 1"] = df_impact.loc[
                     jx, "Total Damages, Adjusted ('000 US$')"
                 ]
                 new_row["Total Deaths 12"] = (
@@ -197,6 +206,6 @@ for ix, row in df_emdat.iterrows():
         continue
 
 # %%
-df.to_csv("df_impacts.csv", sep=";", index=False)
+df.to_csv("df_single_and_pair_impacts.csv", sep=";", index=False)
 
 # %%
