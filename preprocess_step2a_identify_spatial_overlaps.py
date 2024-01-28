e9444c7d791c4ec60227d49130531e01b65cbf9d
Jäger <w.s.jaeger@vu.nl>
2024-01-28 11:41:53 +0100

    rename in step2a

--- preprocess_step2a_identify_spatial_overlaps.py
+++ preprocess_step2a_identify_spatial_overlaps.py
@@ -25,14 +25,14 @@ gdf_impact = gpd.GeoDataFrame(df_impact, crs="epsg:4326")
 
 # %% Create list of indices of all possible event combinations
 event_indices = gdf_impact.index.values
-possible_event_combinations = list(itertools.combinations(event_indices, 2))
+possible_event_pairs = list(itertools.combinations(event_indices, 2))
 
 
 # %%
 def check_intersection(
     possible_event_combination: tuple,
     gdf: gpd.GeoDataFrame,
-    event_combinations: list,
+    event_pairs: list,
     split_events: list,
 ):
     event1 = gdf.loc[[possible_event_combination[0]]]
@@ -41,16 +41,16 @@ def check_intersection(
         if event1.touches(event2, align=False).values[0] > 0:
             split_events.append(possible_event_combination)
         else:
-            event_combinations.append(possible_event_combination)
+            event_pairs.append(possible_event_combination)
 
     # if event1.overlaps(event2, align=False).values[0]:
-    #     event_combinations.append(possible_event_combination)
+    #     event_pairs.append(possible_event_combination)
     #     return
     # if event1.covers(event2, align=False).values[0]:
-    #     event_combinations.append(possible_event_combination)
+    #     event_pairs.append(possible_event_combination)
     #     return
     # if event1.covered_by(event2, align=False).values[0]:
-    #     event_combinations.append(possible_event_combination)
+    #     event_pairs.append(possible_event_combination)
     #     return
     # if event1.touches(event2, align=False).values[0]:
     #     split_events.append(possible_event_combination)
@@ -58,16 +58,16 @@ def check_intersection(
 
 
 # %%
-event_combinations = list()
+event_pairs = list()
 split_events = list()
 
 threads = []
 start = time.time()
-for possible_event_combination in possible_event_combinations:
+for possible_event_combination in possible_event_pairs:
     try:
         thread = threading.Thread(
             target=check_intersection(
-                possible_event_combination, gdf_impact, event_combinations, split_events
+                possible_event_combination, gdf_impact, event_pairs, split_events
             )
         )
         threads.append(thread)
@@ -85,7 +85,7 @@ end = time.time()
 duration = end - start
 
 # %%
-df = pd.DataFrame(event_combinations, columns=["Event1", "Event2"])
+df = pd.DataFrame(event_pairs, columns=["Event1", "Event2"])
 df2 = pd.DataFrame(split_events, columns=["Event1", "Event2"])
 
 df.to_csv("data/event_pairs.csv", sep=";", index=False)
@@ -94,17 +94,15 @@ df2.to_csv("data/split_event_pairs.csv", sep=";", index=False)
 
 # %%
 # df = pd.read_csv("event_pairs.csv", sep=";")
-event_combinations = list(df.itertuples(index=False, name=None))
-unique_events = list(set(itertools.chain.from_iterable(event_combinations)))
+event_pairs = list(df.itertuples(index=False, name=None))
+unique_events = list(set(itertools.chain.from_iterable(event_pairs)))
 
 # %%
 dict_spatially_overlapping_events = {}
 for event in unique_events:
-    filter_event = [event in ec for ec in event_combinations]
+    filter_event = [event in ec for ec in event_pairs]
     overlapping_events = set(
-        itertools.chain.from_iterable(
-            (itertools.compress(event_combinations, filter_event))
-        )
+        itertools.chain.from_iterable((itertools.compress(event_pairs, filter_event)))
     )
     overlapping_events.remove(event)
     dict_spatially_overlapping_events[event] = json.dumps(list(overlapping_events))
